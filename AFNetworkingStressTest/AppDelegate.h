3605fdcf07b6bcb0aefe836727d508360e1eeaad
Erik Olsson <valross2@gmail.com>
2012-02-23 18:22:13 +0100

    First commit

---
+++ AFNetworkingStressTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AFNetworkingStressTest
+//
+//  Created by Erik Olsson on 2/23/12.
+//  Copyright (c) 2012 Traduko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AFNetworkingStressTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AFNetworkingStressTest
+//
+//  Created by Erik Olsson on 2/23/12.
+//  Copyright (c) 2012 Traduko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
